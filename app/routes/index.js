7e281161795c7967962e368a8b0c7b8b2bbeb5b0
Diljeet Singh <jeetds1994@gmail.com>
2017-11-10 19:46:44 -0500

    finished lesson

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 
+  var data = req.app.get("appData");
+  var pagePhotos = [];
+  data.speakers.forEach(function(item){
+    pagePhotos = pagePhotos.concat(item.artwork)
+  })
+
   res.render('index', {
     pageTitle: 'Home',
+    artwork: pagePhotos,
     pageID: 'home'
   });
 
